55ecd59a383022282d8fe046bf5ad6a44936846d
Breezewish <me@breeswish.org>
2014-01-20 15:02:00 +0800

    fix munge detection

--- build/lib/mungedetector.js
+++ build/lib/mungedetector.js
@@ -1,5 +1,5 @@
 (function() {
-  var MungeDetector, Munges, NemesisMethodName, async, detectMungeIndex, getMungeIITC, tryMungeSet;
+  var MungeDetector, Munges, NemesisMethodName, async, extractMunge, tryMungeSet;
 
   async = require('async');
 
@@ -39,22 +39,8 @@
             return callback();
           });
         }, function(callback) {
-          if (Munges.Data === null) {
-            callback();
-            return;
-          }
-          logger.info('[MungeDetector] Trying to use alternative internal munge data.');
-          return detectMungeIndex(function(err) {
-            if (err == null) {
-              callback('new');
-              return;
-            }
-            logger.warn('[MungeDetector] Failed.');
-            return callback();
-          });
-        }, function(callback) {
-          logger.info('[MungeDetector] Trying to parse newest IITC munge data.');
-          return getMungeIITC(function(err) {
+          logger.info('[MungeDetector] Trying to extract munge data from ingress.com/intel.');
+          return extractMunge(function(err) {
             if (err == null) {
               callback('new');
               return;
@@ -130,66 +116,109 @@
     });
   };
 
-  detectMungeIndex = function(callback) {
-    var MAGIC_CODE;
-    MAGIC_CODE = 'nemesis.dashboard.requests.MethodName = ';
+  extractMunge = function(callback) {
     return Request.get('/jsc/gen_dashboard.js', function(error, response, body) {
-      var index, munge, p1, p2, _i, _len, _ref;
+      var err, export_obj, extractMungeFromStock, google, result;
       if (error) {
         callback('fail');
         return;
       }
       body = body.toString();
-      p1 = body.indexOf(MAGIC_CODE);
-      p2 = body.indexOf('}', p1);
-      NemesisMethodName = eval("(" + body.substring(p1 + MAGIC_CODE.length, p2 + 1) + ")");
-      _ref = Munges.Data;
-      for (index = _i = 0, _len = _ref.length; _i < _len; index = ++_i) {
-        munge = _ref[index];
-        if (NemesisMethodName.GET_GAME_SCORE === munge['dashboard.getGameScore']) {
-          Munges.ActiveSet = index;
-          break;
-        }
-      }
-      return tryMungeSet(Munges.Data[Munges.ActiveSet], function(err) {
-        if (err == null) {
-          callback();
-          return;
-        }
-        return callback('fail');
-      });
-    });
-  };
-
-  getMungeIITC = function(callback) {
-    var MAGIC_CODE, needle;
-    MAGIC_CODE = 'window.requestParameterMunges = ';
-    needle = require('needle');
-    return needle.get('https://secure.jonatkins.com/iitc/release/total-conversion-build.user.js', {
-      compressed: true,
-      timeout: 20000
-    }, function(error, response, body) {
-      var MungeSet, index, munge, p1, p2, _i, _len;
-      if (error) {
-        callback('fail');
-        return;
-      }
-      body = body.toString();
-      p1 = body.indexOf(MAGIC_CODE);
-      p2 = body.indexOf(']', p1);
-      MungeSet = eval("(" + body.substring(p1 + MAGIC_CODE.length, p2 + 1) + ")");
-      Munges.Data = MungeSet;
-      if (NemesisMethodName !== null) {
-        for (index = _i = 0, _len = MungeSet.length; _i < _len; index = ++_i) {
-          munge = MungeSet[index];
-          if (NemesisMethodName.GET_GAME_SCORE === munge['dashboard.getGameScore']) {
-            Munges.ActiveSet = index;
-            break;
+      export_obj = {};
+      google = {
+        maps: {
+          OverlayView: function() {
+            return null;
           }
         }
-      } else {
-        Munges.ActiveSet = Munges.Data.length - 1;
+      };
+      eval(body + ';export_obj.nemesis = nemesis;');
+      extractMungeFromStock = function(nemesis) {
+            var foundMunges = {};
+
+            // these are easy - directly available in variables
+            // NOTE: the .toString() is there so missing variables throw an exception, rather than storing 'undefined'
+            foundMunges['dashboard.getArtifactInfo'] = nemesis.dashboard.requests.MethodName.GET_ARTIFACT_INFO.toString();
+            foundMunges['dashboard.getGameScore'] = nemesis.dashboard.requests.MethodName.GET_GAME_SCORE.toString();
+            foundMunges['dashboard.getPaginatedPlexts'] = nemesis.dashboard.requests.MethodName.GET_PAGINATED_PLEXTS.toString();
+            foundMunges['dashboard.getThinnedEntities'] = nemesis.dashboard.requests.MethodName.GET_THINNED_ENTITIES.toString();
+            foundMunges['dashboard.getPortalDetails'] = nemesis.dashboard.requests.MethodName.GET_PORTAL_DETAILS.toString();
+            foundMunges['dashboard.redeemReward'] = nemesis.dashboard.requests.MethodName.REDEEM_REWARD.toString();
+            foundMunges['dashboard.sendInviteEmail'] = nemesis.dashboard.requests.MethodName.SEND_INVITE_EMAIL.toString();
+            foundMunges['dashboard.sendPlext'] = nemesis.dashboard.requests.MethodName.SEND_PLEXT.toString();
+
+            // the rest are trickier - we need to parse the functions of the stock site. these break very often
+            // on site updates
+
+            // regular expression - to match either x.abcdef123456wxyz or x["123456abcdefwxyz"] format for property access
+            var mungeRegExpProp = '(?:\\.([a-z][a-z0-9]{15})|\\["([0-9][a-z0-9]{15})"\\])';
+            // and one to match members of object literal initialisation - {abcdef123456wxyz: or {"123456abcdefwxyz":
+            var mungeRegExpLit = '(?:([a-z][a-z0-9]{15})|"([0-9][a-z0-9]{15})"):';
+
+            // common parameters - method, version, version_parameter - currently found in the 
+            // nemesis.dashboard.network.XhrController.prototype.doSendRequest_ function
+            // look for something like
+            //  var e = a.getData();
+            //  e["3sld77nsm0tjmkvi"] = c;
+            //  e.xz7q6r3aja5ttvoo = "b121024077de2a0dc6b34119e4440785c9ea5e64";
+            var reg = new RegExp('getData\\(\\);.*\\n.*'+mungeRegExpProp+' =.*\n.*'+mungeRegExpProp+' *= *"([a-z0-9]{40})','m');
+            var result = reg.exec(nemesis.dashboard.network.XhrController.prototype.doSendRequest_.toString());
+            // there's two ways of matching the munge expression, so try both
+            foundMunges.method = result[1] || result[2];
+            foundMunges.version = result[3] || result[4];
+            foundMunges.version_parameter = result[5];
+
+            // GET_THINNED_ENTITIES parameters
+            var reg = new RegExp('GET_THINNED_ENTITIES, {'+mungeRegExpLit+'[a-z]');
+            var result = reg.exec(nemesis.dashboard.network.DataFetcher.prototype.getGameEntities.toString());
+            foundMunges.quadKeys = result[1] || result[2];
+
+            // GET_PAGINATED_PLEXTS
+            var reg = new RegExp('GET_PAGINATED_PLEXTS, [a-z] = [a-z] \\|\\| nemesis.dashboard.BoundsParams.getBoundsParamsForWorld\\(\\), [a-z] = [a-z] \\|\\| -1, [a-z] = [a-z] \\|\\| -1, [a-z] = {'+mungeRegExpLit+'[a-z], '+mungeRegExpLit+'Math.round\\([a-z].bounds.sw.lat\\(\\) \\* 1E6\\), '+mungeRegExpLit+'Math.round\\([a-z].bounds.sw.lng\\(\\) \\* 1E6\\), '+mungeRegExpLit+'Math.round\\([a-z].bounds.ne.lat\\(\\) \\* 1E6\\), '+mungeRegExpLit+'Math.round\\([a-z].bounds.ne.lng\\(\\) \\* 1E6\\), '+mungeRegExpLit+'[a-z], '+mungeRegExpLit+'[a-z]};\n *[a-z]'+mungeRegExpProp+' = [a-z];\n *[a-z] > -1 && \\([a-z]'+mungeRegExpProp+' = true\\);', 'm');
+            var result = reg.exec(nemesis.dashboard.network.PlextStore.prototype.getPlexts.toString());
+
+            foundMunges.desiredNumItems = result[1] || result[2];
+            
+            foundMunges.minLatE6 = result[3] || result[4];
+            foundMunges.minLngE6 = result[5] || result[6];
+            foundMunges.maxLatE6 = result[7] || result[8];
+            foundMunges.maxLngE6 = result[9] || result[10];
+            foundMunges.minTimestampMs = result[11] || result[12];
+            foundMunges.maxTimestampMs = result[13] || result[14];
+            foundMunges.chatTab = result[15] || result[16];  //guessed parameter name - only seen munged
+            foundMunges.ascendingTimestampOrder = result[17] || result[18];
+
+            // SEND_PLEXT
+            var reg = new RegExp('SEND_PLEXT, {'+mungeRegExpLit+'[a-z], '+mungeRegExpLit+'[a-z], '+mungeRegExpLit+'[a-z], '+mungeRegExpLit+'[a-z]}');
+            var result = reg.exec(nemesis.dashboard.network.PlextStore.prototype.sendPlext.toString());
+
+            foundMunges.message = result[1] || result[2];
+            foundMunges.latE6 = result[3] || result[4];
+            foundMunges.lngE6 = result[5] || result[6];
+            var chatTab = result[7] || result[8];
+            if (chatTab != foundMunges.chatTab) throw 'Error: inconsistent munge parsing for chatTab';
+
+            // GET_PORTAL_DETAILS
+            var reg = new RegExp('GET_PORTAL_DETAILS, {'+mungeRegExpLit+'a}');
+            var result = reg.exec(nemesis.dashboard.network.DataFetcher.prototype.getPortalDetails.toString());
+
+            foundMunges.guid = result[1] || result[2];
+
+            // SEND_INVITE_EMAIL
+            var reg = new RegExp('SEND_INVITE_EMAIL, {'+mungeRegExpLit+'b}');
+            foundMunges.inviteeEmailAddress = result[1] || result[2];
+
+            return foundMunges;
+        };
+      try {
+        result = extractMungeFromStock(export_obj.nemesis);
+      } catch (_error) {
+        err = _error;
+        callback('fail');
+        return;
       }
+      Munges.Data = [result];
+      Munges.ActiveSet = 0;
       return tryMungeSet(Munges.Data[Munges.ActiveSet], function(err) {
         if (err == null) {
           callback();
--- src/lib/mungedetector.coffee
+++ src/lib/mungedetector.coffee
@@ -48,31 +48,11 @@ MungeDetector = GLOBAL.MungeDetector =
 
             , (callback) ->
 
-                # 2. get munge-index from JavaScript online
+                # 2. extract munge data from Ingress.com/intel
 
-                # No munges in database: skip this step
-                if Munges.Data is null
-                    callback()
-                    return
+                logger.info '[MungeDetector] Trying to extract munge data from ingress.com/intel.'
 
-                logger.info '[MungeDetector] Trying to use alternative internal munge data.'
-
-                detectMungeIndex (err) ->
-
-                    if not err?
-                        callback 'new'
-                        return
-
-                    logger.warn '[MungeDetector] Failed.'
-                    callback()
-
-            , (callback) ->
-
-                # 3. get munge data from IITC
-
-                logger.info '[MungeDetector] Trying to parse newest IITC munge data.'
-
-                getMungeIITC (err) ->
+                extractMunge (err) ->
 
                     if not err?
                         callback 'new'
@@ -154,70 +134,112 @@ tryMungeSet = (munge, callback) ->
         
         task.success && task.success body
 
-detectMungeIndex = (callback) ->
-
-    MAGIC_CODE = 'nemesis.dashboard.requests.MethodName = '
+extractMunge = (callback) ->
 
     Request.get '/jsc/gen_dashboard.js', (error, response, body) ->
-
+        
         if error
             callback 'fail'
             return
 
         body = body.toString()
-        p1 = body.indexOf MAGIC_CODE
-        p2 = body.indexOf '}', p1
 
-        NemesisMethodName = eval("(" + body.substring(p1 + MAGIC_CODE.length, p2 + 1) + ")")
-        
-        for munge, index in Munges.Data
-            if NemesisMethodName.GET_GAME_SCORE is munge['dashboard.getGameScore']
-                Munges.ActiveSet = index
-                break
+        # some hacks
+        export_obj = {}
+        google =
+            maps:
+                OverlayView: ->
+                    null
+        eval body + ';export_obj.nemesis = nemesis;'
 
-        # test it
-        tryMungeSet Munges.Data[Munges.ActiveSet], (err) ->
+        # from IITC
+        extractMungeFromStock = `function(nemesis) {
+            var foundMunges = {};
 
-            if not err?
-                callback()
-                return
+            // these are easy - directly available in variables
+            // NOTE: the .toString() is there so missing variables throw an exception, rather than storing 'undefined'
+            foundMunges['dashboard.getArtifactInfo'] = nemesis.dashboard.requests.MethodName.GET_ARTIFACT_INFO.toString();
+            foundMunges['dashboard.getGameScore'] = nemesis.dashboard.requests.MethodName.GET_GAME_SCORE.toString();
+            foundMunges['dashboard.getPaginatedPlexts'] = nemesis.dashboard.requests.MethodName.GET_PAGINATED_PLEXTS.toString();
+            foundMunges['dashboard.getThinnedEntities'] = nemesis.dashboard.requests.MethodName.GET_THINNED_ENTITIES.toString();
+            foundMunges['dashboard.getPortalDetails'] = nemesis.dashboard.requests.MethodName.GET_PORTAL_DETAILS.toString();
+            foundMunges['dashboard.redeemReward'] = nemesis.dashboard.requests.MethodName.REDEEM_REWARD.toString();
+            foundMunges['dashboard.sendInviteEmail'] = nemesis.dashboard.requests.MethodName.SEND_INVITE_EMAIL.toString();
+            foundMunges['dashboard.sendPlext'] = nemesis.dashboard.requests.MethodName.SEND_PLEXT.toString();
 
-            callback 'fail'
+            // the rest are trickier - we need to parse the functions of the stock site. these break very often
+            // on site updates
 
-getMungeIITC = (callback) ->
+            // regular expression - to match either x.abcdef123456wxyz or x["123456abcdefwxyz"] format for property access
+            var mungeRegExpProp = '(?:\\.([a-z][a-z0-9]{15})|\\["([0-9][a-z0-9]{15})"\\])';
+            // and one to match members of object literal initialisation - {abcdef123456wxyz: or {"123456abcdefwxyz":
+            var mungeRegExpLit = '(?:([a-z][a-z0-9]{15})|"([0-9][a-z0-9]{15})"):';
 
-    MAGIC_CODE = 'window.requestParameterMunges = '
+            // common parameters - method, version, version_parameter - currently found in the 
+            // nemesis.dashboard.network.XhrController.prototype.doSendRequest_ function
+            // look for something like
+            //  var e = a.getData();
+            //  e["3sld77nsm0tjmkvi"] = c;
+            //  e.xz7q6r3aja5ttvoo = "b121024077de2a0dc6b34119e4440785c9ea5e64";
+            var reg = new RegExp('getData\\(\\);.*\\n.*'+mungeRegExpProp+' =.*\n.*'+mungeRegExpProp+' *= *"([a-z0-9]{40})','m');
+            var result = reg.exec(nemesis.dashboard.network.XhrController.prototype.doSendRequest_.toString());
+            // there's two ways of matching the munge expression, so try both
+            foundMunges.method = result[1] || result[2];
+            foundMunges.version = result[3] || result[4];
+            foundMunges.version_parameter = result[5];
 
-    needle = require 'needle'
-    needle.get 'https://secure.jonatkins.com/iitc/release/total-conversion-build.user.js', 
+            // GET_THINNED_ENTITIES parameters
+            var reg = new RegExp('GET_THINNED_ENTITIES, {'+mungeRegExpLit+'[a-z]');
+            var result = reg.exec(nemesis.dashboard.network.DataFetcher.prototype.getGameEntities.toString());
+            foundMunges.quadKeys = result[1] || result[2];
 
-        compressed: true
-        timeout:    20000
+            // GET_PAGINATED_PLEXTS
+            var reg = new RegExp('GET_PAGINATED_PLEXTS, [a-z] = [a-z] \\|\\| nemesis.dashboard.BoundsParams.getBoundsParamsForWorld\\(\\), [a-z] = [a-z] \\|\\| -1, [a-z] = [a-z] \\|\\| -1, [a-z] = {'+mungeRegExpLit+'[a-z], '+mungeRegExpLit+'Math.round\\([a-z].bounds.sw.lat\\(\\) \\* 1E6\\), '+mungeRegExpLit+'Math.round\\([a-z].bounds.sw.lng\\(\\) \\* 1E6\\), '+mungeRegExpLit+'Math.round\\([a-z].bounds.ne.lat\\(\\) \\* 1E6\\), '+mungeRegExpLit+'Math.round\\([a-z].bounds.ne.lng\\(\\) \\* 1E6\\), '+mungeRegExpLit+'[a-z], '+mungeRegExpLit+'[a-z]};\n *[a-z]'+mungeRegExpProp+' = [a-z];\n *[a-z] > -1 && \\([a-z]'+mungeRegExpProp+' = true\\);', 'm');
+            var result = reg.exec(nemesis.dashboard.network.PlextStore.prototype.getPlexts.toString());
 
-    , (error, response, body) ->
+            foundMunges.desiredNumItems = result[1] || result[2];
+            
+            foundMunges.minLatE6 = result[3] || result[4];
+            foundMunges.minLngE6 = result[5] || result[6];
+            foundMunges.maxLatE6 = result[7] || result[8];
+            foundMunges.maxLngE6 = result[9] || result[10];
+            foundMunges.minTimestampMs = result[11] || result[12];
+            foundMunges.maxTimestampMs = result[13] || result[14];
+            foundMunges.chatTab = result[15] || result[16];  //guessed parameter name - only seen munged
+            foundMunges.ascendingTimestampOrder = result[17] || result[18];
 
-        if error
+            // SEND_PLEXT
+            var reg = new RegExp('SEND_PLEXT, {'+mungeRegExpLit+'[a-z], '+mungeRegExpLit+'[a-z], '+mungeRegExpLit+'[a-z], '+mungeRegExpLit+'[a-z]}');
+            var result = reg.exec(nemesis.dashboard.network.PlextStore.prototype.sendPlext.toString());
+
+            foundMunges.message = result[1] || result[2];
+            foundMunges.latE6 = result[3] || result[4];
+            foundMunges.lngE6 = result[5] || result[6];
+            var chatTab = result[7] || result[8];
+            if (chatTab != foundMunges.chatTab) throw 'Error: inconsistent munge parsing for chatTab';
+
+            // GET_PORTAL_DETAILS
+            var reg = new RegExp('GET_PORTAL_DETAILS, {'+mungeRegExpLit+'a}');
+            var result = reg.exec(nemesis.dashboard.network.DataFetcher.prototype.getPortalDetails.toString());
+
+            foundMunges.guid = result[1] || result[2];
+
+            // SEND_INVITE_EMAIL
+            var reg = new RegExp('SEND_INVITE_EMAIL, {'+mungeRegExpLit+'b}');
+            foundMunges.inviteeEmailAddress = result[1] || result[2];
+
+            return foundMunges;
+        }`
+
+        try
+            result = extractMungeFromStock export_obj.nemesis
+        catch err
             callback 'fail'
             return
 
-        body = body.toString()
-        p1 = body.indexOf MAGIC_CODE
-        p2 = body.indexOf ']', p1
+        Munges.Data      = [result]
+        Munges.ActiveSet = 0
 
-        MungeSet = eval("(" + body.substring(p1 + MAGIC_CODE.length, p2 + 1) + ")")
-        Munges.Data = MungeSet
-
-        if NemesisMethodName isnt null
-
-            for munge, index in MungeSet
-                if NemesisMethodName.GET_GAME_SCORE is munge['dashboard.getGameScore']
-                    Munges.ActiveSet = index
-                    break
-
-        else
-
-            Munges.ActiveSet = Munges.Data.length - 1
-        
         # test it
         tryMungeSet Munges.Data[Munges.ActiveSet], (err) ->
 
